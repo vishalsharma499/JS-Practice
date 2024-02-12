c5124e5e4b518812074e7804eff169999fbcbbed
vishalsharma499 <70803219+vishalsharma499@users.noreply.github.com>
2024-02-12 15:47:04 +0000

    for of

---
+++ 04_Iterations/three.js
@@ -0,0 +1,34 @@
+// for of
+
+const arr=[1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greeting="hello world"
+for (const greet of greeting) {
+    // console.log(`Each char is ${greet}`);
+}
+
+// Maps It is gives the unique value remove duplicate values
+
+coap.set('FR',"France")
+map.set('IN',"India")
+// console.log(map);
+for (const [key,value] of map) {
+    console.log(key, ':-',value);
+}
+nst map = new Map ()
+map.set('IN',"India")
+map.set('USA',"United state of america")
+m
+// not working 
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
