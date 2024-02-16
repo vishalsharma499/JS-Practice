18139ff9b6a033f05d89aa125714cab02aff8cb2
vishalsharma499 <70803219+vishalsharma499@users.noreply.github.com>
2024-02-16 18:05:24 +0000

    math pi

---
+++ 09_classes_and_oop/mathpi.js
@@ -0,0 +1,35 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5 // we can not change pi value because in js writable property false 
+// console.log(Math.PI);
+
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
